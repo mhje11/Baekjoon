9e2652f4f4e5c1418280e23c35354f4381407ece
mhje11 <mhje1122@gmail.com>
2024-03-21 17:11:03 +0900

    S_4_1920 sort

--- src/B3/B_3_5597.java
+++ src/B3/B_3_5597.java
@@ -5,16 +5,12 @@ import java.util.Scanner;
 public class B_3_5597 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean[] arr = new boolean[32];
-        int N = 0;
+        boolean[] arr = new boolean[31];
 
-        while (N < 28) {
-            int number = sc.nextInt();
-            arr[number] = true;
-            N++;
+        for (int i = 0; i < 28; i++) {
+            int x = sc.nextInt();
+            arr[x] = true;
         }
-
-
         for (int i = 1; i <= 30; i++) {
             if (!arr[i]) {
                 System.out.println(i);
